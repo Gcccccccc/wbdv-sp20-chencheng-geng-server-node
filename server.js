d7104ed3bd9a9041ea1ed5491817c5a3818bc224
Gcc <gcc@Gccs-MBP.verizon.net>
2020-04-01 20:49:27 -0400

    node server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+require('./controllers/quiz.controller.server')(app)
+require('./controllers/question.controller.server')(app)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
