0feca2371e29ae45b0cb7b25eaa111deec555208
Gcc <gcc@Gccs-MBP.verizon.net>
2020-04-11 23:37:42 -0400

    controllers

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-require('./controllers/quiz.controller.server')(app)
-require('./controllers/question.controller.server')(app)
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+require('./controllers/quizzes.controller.server')(app)
+require('./controllers/questions.controller.server')(app)
+require('./controllers/quiz-attempts.controller.server')(app)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
